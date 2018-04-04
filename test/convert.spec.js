749bcd05e357d18b66da806bd695017c9b2b8913
David Dias <daviddias.p@gmail.com>
2018-04-04 14:23:26 +0100

    chore: fix linting

--- test/convert.spec.js
+++ test/convert.spec.js
@@ -7,7 +7,7 @@ const ethCID = require('../src')
 describe('eth hash to CID', () => {
   it('convert Buffer hash to CID', () => {
     const block =
-      new Buffer('0100000000000000000000000000000000000000000000000000000000000000', 'hex')
+      Buffer.from('0100000000000000000000000000000000000000000000000000000000000000', 'hex')
     const cid = ethCID('eth-block', block)
     expect(cid.toBaseEncodedString())
       .to.equal('z43AaGErnhsnmexJRDrxYum9LFok5vrhNF4k3EPQpRXqheXkWAj')
